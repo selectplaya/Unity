9e6a05b02cdc6a58408f82b0d5f9fb6cb70d89d1
Timothy Fuller <timmyfuller1@gmail.com>
2018-11-14 21:48:05 +0000

    best fit text added quit

---
+++ MemBLocks/Assets/Scripts/LoadScene.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadScene : MonoBehaviour {
+
+	//// Use this for initialization
+	//void Start () {
+		
+	//}
+	
+	//// Update is called once per frame
+	//void Update () {
+		
+	//}
+    public void LoadByIndex(int sceneIndex)
+    {
+        SceneManager.LoadScene(sceneIndex); 
+    }
+    public void LoadByName(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
